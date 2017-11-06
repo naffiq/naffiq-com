836f0c3225c927d172fa13ff10ebbf64e9d592b4
naffiq <naffiq@gmail.com>
2017-11-07 03:25:46 +0600

    Added express.js server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+app.prepare()
+.then(() => {
+    const server = express();
+
+    server.get('/post/:slug', (req, res) => {
+        const actualPage = '/post';
+        const queryParams = {slug: req.params.slug};
+        console.log('queryParams', queryParams);
+        app.render(req, res, actualPage, queryParams);
+    })
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(PORT, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${PORT}`);
+    })
+})
+.catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+});
