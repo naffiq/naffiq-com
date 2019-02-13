e92bec8585ddb79f7b881f7e8001e285663dd2e8
naffiq <abdu.galymzhan@gmail.com>
2019-02-13 09:17:25 +0100

    fix(useMobileWidthEffect): fix window reference

--- src/hooks/useMobileWidthEffect.tsx
+++ src/hooks/useMobileWidthEffect.tsx
@@ -2,23 +2,26 @@ import * as React from "react";
 
 export const MAX_MOBILE_WIDTH = 767;
 
+// Gatsby build fix
+const myWindow = (typeof window !== "undefined" && window) || ({} as any);
+
 const useMobileWidthEffect = (callback: (isMobile: boolean) => void) => {
-  const [isMobile, setIsMobile] = React.useState(window.innerWidth < 767);
+  const [isMobile, setIsMobile] = React.useState(myWindow.innerWidth < 767);
 
   React.useEffect(() => {
     const checkIfMobile = () => {
-      if (window.innerWidth >= MAX_MOBILE_WIDTH && isMobile) {
+      if (myWindow.innerWidth >= MAX_MOBILE_WIDTH && isMobile) {
         setIsMobile(false);
         callback(false);
-      } else if (window.innerWidth < MAX_MOBILE_WIDTH && !isMobile) {
+      } else if (myWindow.innerWidth < MAX_MOBILE_WIDTH && !isMobile) {
         callback(true);
         setIsMobile(true);
       }
     };
 
-    window.addEventListener("resize", checkIfMobile);
+    myWindow.addEventListener("resize", checkIfMobile);
 
-    return () => window.removeEventListener("resize", checkIfMobile);
+    return () => myWindow.removeEventListener("resize", checkIfMobile);
   });
 
   return isMobile;
