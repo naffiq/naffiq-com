b4f9ebfdf26f13f6486e6f5904b3c214e8e012a1
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2024-01-23 20:01:35 +0600

    feat(perne): updates for the upcoming app

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
+import DefaultLayout from '../layouts'
+import { ContainerContent } from '../components/Container'
 
 const NotFoundPage = () => (
-  <div>
+  <DefaultLayout>
+    <ContainerContent>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    </ContainerContent>
+  </DefaultLayout>
 )
 
 export default NotFoundPage
---
+++ src/pages/perne.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+import Container, { ContainerContent } from "../components/Container";
+import DefaultLayout from "../layouts";
+import { LineThrough, Paragraph } from "../components/Typography";
+import Link, { A } from "../components/Link";
+
+const PernePage = () => (
+  <DefaultLayout>
+    <Container narrow>
+      <ContainerContent>
+        <h1>Perne</h1>
+        <Paragraph>
+          Perne – қазақ тіліне арналған қателерді дұрыстайтын iOS пернетақтасы.
+        </Paragraph>
+        <Paragraph>
+          Сондай қосымшаны жазу идясы бұрыннан пайда болған, бірақ Swift тілді жаңа ғана үйрендім.
+          Сол үшін қосымшада қателер болуы мүмкін. Қателер тұралы осы поштаға есеп бере аласыз 
+          <A href="mailto:perne@naffiq.com" target="_blank">perne@naffiq.com</A>.
+        </Paragraph>
+        <Paragraph>
+          Қазақша қателерді табсаңыз да жібереберіңдер 😉
+        </Paragraph>
+        <Paragraph>
+          Perne – это клавиатура для iOS, которая помогает печатать без ошибок на казахском языке.
+        </Paragraph>
+        <Paragraph>
+          Идея появилась давно, а вот умение разрабатывать приложения на Swift относительно недавно. 
+          Поэтому приложение может иметь некоторые ошибки, о которых вы можете писать на 
+          <A href="mailto:perne@naffiq.com" target="_blank">perne@naffiq.com</A>, чем вы мне очень поможете. 
+        </Paragraph>
+        <Paragraph>
+          Так же можете написать об ошибках, которые я мог допустить на казахском 😉
+        </Paragraph>
+      </ContainerContent>
+    </Container>
+  </DefaultLayout>
+);
+
+export default PernePage;
